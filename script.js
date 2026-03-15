ed533e1d7dc2216f25bbbf549df63273ec9a70a1
Raul <a90822@alunos.uminho.pt>
2026-03-15 23:02:05 +0000

    acrescentei o mapa de braga com os devidos pontos no script e no index

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ console.log('Website TUB Projeto carregado com sucesso!');
 document.querySelector('.btn1').addEventListener('click', function() {
     alert('Conseguiste');
 });
+
+///////////////////////////////MAPA DE BRAGA///////////////////////////
+document.addEventListener("DOMContentLoaded", function () {
+
+    const map = L.map("map").setView([41.5454, -8.4265], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; OpenStreetMap contributors"
+    }).addTo(map);
+
+    const panels = [
+    { id: 1, location: "Avenida Central", lat: 41.5501, lon: -8.4213 },
+    { id: 2, location: "Universidade do Minho", lat: 41.5607, lon: -8.3975 },
+    { id: 3, location: "Braga Parque", lat: 41.5582, lon: -8.4048 },
+    { id: 4, location: "Estação de Braga", lat: 41.5471, lon: -8.4343 },
+    { id: 5, location: "Hospital de Braga", lat: 41.5618, lon: -8.3996 },
+
+    { id: 6, location: "Bom Jesus", lat: 41.5547, lon: -8.3772 },
+    
+];
+
+    panels.forEach(panel => {
+        L.marker([panel.lat, panel.lon])
+            .addTo(map)
+            .bindPopup(panel.location);
+    });
+
+});
+/////////////////////////////////////////////FIM MAPA DE braga//////////////////////////////////////////
