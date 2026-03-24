b00e7d14904bcc9ec4280bb9ae9a4e69a1a60ffb
SimaoCosta0406 <simaocosta06@sapo.pt>
2026-03-24 18:30:07 +0000

    integração do projeto com um link URL

---
+++ src/main/java/dai/boot/projeto/ProjetoApplication.java
@@ -0,0 +1,13 @@
+package dai.boot.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Classe principal do Spring Boot
+@SpringBootApplication
+public class ProjetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoApplication.class, args);
+    }
+}
---
+++ src/test/java/dai/boot/projeto/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package dai.boot.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
